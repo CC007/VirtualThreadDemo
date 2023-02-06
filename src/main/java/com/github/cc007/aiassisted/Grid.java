2829c2746118cab3a33c41d03259f873ad0f048c
CC007 <Coolcat_the_best2@Hotmail.com>
2023-02-06 23:46:15 +0100

    Randomly initialize the grid.

--- src/main/java/com/github/cc007/aiassisted/Grid.java
+++ src/main/java/com/github/cc007/aiassisted/Grid.java
@@ -1,11 +1,14 @@
 package com.github.cc007.aiassisted;
 
+import java.util.Random;
+
 public record Grid(int width, int height, Cell[][] cells) {
     public static Grid of(int width, int height) {
         Cell[][] cells = new Cell[height][width];
+        Random random = new Random();
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                cells[i][j] = new Cell(false);
+                cells[i][j] = new Cell(random.nextBoolean());
             }
         }
         return new Grid(width, height, cells);
